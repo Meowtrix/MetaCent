38a56e48b0abccdb0132b2bcfc95d6f2af74aa85
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2017-11-02 22:27:57 +0800

    Basic metadata setting.

--- MetaCent/Properties/AssemblyInfo.cs
+++ MetaCent/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MetaCent")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Metadata centered gallery manager")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MetaCent")]
